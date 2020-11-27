91eac7115d5cbc1154289e3823e3a778ff32acd4
vamsivardhanreddy <mrvamsireddy@gmail.com>
2020-11-27 23:54:08 +0530

    message

--- src/components/signup/signup.jsx
+++ src/components/signup/signup.jsx
@@ -1,8 +1,34 @@
 import React, {Component} from 'react';
-
+import {Input} from 'semantic-ui-react';
+import './Signup.css';
 export class Signup extends Component {
   render() {
-    return <div>Signup</div>;
+    return (
+      <div className="Signup">
+        <div className="container">
+          <Input
+            className="username"
+            icon="user"
+            iconPosition="left"
+            placeholder="Username"
+          />{' '}
+          <br />
+          <Input
+            className="email"
+            icon="mail"
+            iconPosition="left"
+            placeholder="Email"
+          />{' '}
+          <br />
+          <Input
+            className="password"
+            icon="lock"
+            iconPosition="left"
+            placeholder="Password"
+          />
+        </div>
+      </div>
+    );
   }
 }
 
